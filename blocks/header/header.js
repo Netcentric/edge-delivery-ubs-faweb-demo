4ffbc9352685cecbb78c9bed84f2f8895711ea19
dragos.stefanache <dragos.stefanache@netcentric.biz>
2024-12-10 11:16:23 +0200

    Adds changes based on code review

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -117,6 +117,7 @@ export default async function decorate(block) {
   block.textContent = '';
   const nav = document.createElement('nav');
   nav.id = 'nav';
+  nav.classList.add('nav');
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
   const classes = ['brand', 'sections', 'tools'];
